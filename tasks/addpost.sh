7b835833a24d44083c7c9ea5c16d21831a156768
Jorijn Schrijvershof <jorijn@jorijn.com>
2011-09-21 16:12:32 +0200

    Gestript en geHTML5-iseerd.

--- tasks/addpost.sh
+++
@@ -1,6 +0,0 @@
-rm -Rf /Users/mijndert/Sites/mijndertstuij-nl
-
-cd /Users/mijndert/Sites/blog; jekyll
-
-rsync -avz --delete -e 'ssh -p222' /Users/mijndert/Sites/mijndertstuij-nl/ mijndert@mijndertstuij.nl:/var/www/mijndertstuij-nl
-
