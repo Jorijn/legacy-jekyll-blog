a06fb304a673252b995f34bbe95a0997b9f381f1
Jorijn Schrijvershof <jorijn@jorijn.com>
2011-09-21 21:50:53 +0200

    Eerste concept on twerp

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('.small_post').each(function(ndx, val) { 
-		if (ndx % 3 == 0) 
-			$(this).before($('<div />').addClass('clearfix')); 
+		// if (ndx % 3 == 0) 
+			// $(this).before($('<div />').addClass('clearfix')); 
 	});
 });
