47fb599064a4c99602679a2f1eea65cede8fc41f
Jorijn Schrijvershof <jorijn@jorijn.com>
2011-09-22 13:25:20 +0200

    Added visitor tracking

--- _tasks/generate_sync.sh
+++ _tasks/generate_sync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /Users/jorijn/Documents/Blog && jekyll && \
-s3cmd --delete-removed --reduced-redundancy --guess-mime-type sync /Library/WebServer/Documents/blog/ s3://blog.jorijn.com/
+s3cmd --delete-removed --reduced-redundancy --guess-mime-type sync /Library/WebServer/Documents/blog/ s3://blog.jorijn.com/ && \
+curl 'http://google.com/webmasters/sitemaps/ping?sitemap=http://blog.jorijn.com/sitemap.xml'
 
