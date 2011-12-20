d674d474b08d9986fa6cf9076b8520ca4dfbbbe5
Jorijn Schrijvershof <jorijn@jorijn.com>
2011-12-20 19:01:21 +0100

    Task execute script updated

--- _tasks/generate_sync.sh
+++ _tasks/generate_sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /Users/jorijn/Documents/Blog && jekyll && \
+cd /Users/jorijn/Documents/Blog && jekyll --lsi && \
 s3cmd --delete-removed --reduced-redundancy --guess-mime-type sync /Library/WebServer/Documents/blog/ s3://blog.jorijn.com/ && \
 curl 'http://google.com/webmasters/sitemaps/ping?sitemap=http://blog.jorijn.com/sitemap.xml'
 
